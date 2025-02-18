636385e0a07f6631d941127edd7cd66c81f0b05a
PillowGit <goldentaco727@gmail.com>
2025-02-18 11:37:47 -0800

    Fix ping command

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -5,8 +5,7 @@ module.exports = {
     .setName("ping")
     .setDescription("Reports the bots ping"),
   async execute(interaction, client, storage) {
-    const ms_diff =
-      new Date().getMilliseconds() - interaction.createdAt.getMilliseconds();
+    const ms_diff = new Date().getTime() - interaction.createdAt.getTime();
     await interaction.reply({
       content: ms_diff.toString().concat("ms"),
       ephemeral: true,
